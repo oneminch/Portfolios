8b705908c8b455e20c2d2a578132f5e4745d70c5
oneminch <27751688+oneminch@users.noreply.github.com>
2020-04-01 14:23:12 -0500

    Update Experience: OpenGenus experience date

--- js/main.js
+++ js/main.js
@@ -1,87 +1,96 @@
-const all_exps = document.querySelectorAll(".experience__list__item");  // List of all experiences
-const current_exp = document.querySelector(".experience__current");     // My current up to date experience
-const footer_text = document.querySelector(".footer__text");            // Footer text with copyright content
+const all_exps = document.querySelectorAll(".experience__list__item"); // List of all experiences
+const current_exp = document.querySelector(".experience__current"); // My current up to date experience
+const footer_text = document.querySelector(".footer__text"); // Footer text with copyright content
 const projects__container = document.querySelector(".projects__container");
 
 const data_json = {
-   // Experience data list
-   // Each data: id, name, date, description & link(?)
-   exp_data: [
-      {
-         id: 1,
-         name: "Freelance Web Designer 📱",
-         date: ["Sep '17","Nov '17"],
-         desc: "I helped design and develop a landing page as well as a logo for an app by independent iOS developer.",
-         link: [/* "DDG", "https://ddg.gg" */]
-      },
-      {
-         id: 2,
-         name: "Mini-Projects 💻",
-         date: ["Jul '17","Present"],
-         desc: "I like to work on small projects to solidify new concepts I'm learning.",
-         link: ["@GitHub", "https://github.com/oneminch?tab=repositories"]
-      },
-      {
-         id: 3,
-         name: "Tennessee Tech 🎓",
-         date: ["Aug '17","Dec '21 (exp.)"],
-         desc: "I'm currently a junior at Tennessee Tech University majoring in computer science; I transferred half-way from a 2 year college at which I majored in Information Management Systems.",
-         link: ["Homepage", "https://tntech.edu"]
-      },
-      {
-         id: 4, 
-         name: "OpenGenus Foundation 📝",
-         date: ["Sep '19","Present"],
-         desc: "I'm currently a Software Developer Intern at the OpenGenus Foundation. At the moment, I'm mainly involved in writing articles related to various web development topics", 
-         link: ["Profile", "https://iq.opengenus.org/author/durg"]
-      }
-   ],
+	// Experience data list
+	// Each data: id, name, date, description & link(?)
+	exp_data: [
+		{
+			id: 1,
+			name: "Freelance Web Designer 📱",
+			date: ["Sep '17", "Nov '17"],
+			desc:
+				"I helped design and develop a landing page as well as a logo for an app by independent iOS developer.",
+			link: [
+				/* "DDG", "https://ddg.gg" */
+			]
+		},
+		{
+			id: 2,
+			name: "Mini-Projects 💻",
+			date: ["Jul '17", "Present"],
+			desc:
+				"I like to work on small projects to solidify new concepts I'm learning.",
+			link: ["@GitHub", "https://github.com/oneminch?tab=repositories"]
+		},
+		{
+			id: 3,
+			name: "OpenGenus Foundation 📝",
+			date: ["Sep '19", "Feb '20"],
+			desc:
+				"As a Software Developer Intern at the OpenGenus Foundation, I was mainly involved in writing articles related to various web development topics and help in the development of a web extension for the service.",
+			link: ["Profile", "https://iq.opengenus.org/author/durg"]
+		},
+		{
+			id: 4,
+			name: "Tennessee Tech 🎓",
+			date: ["Aug '17", "Dec '21 (exp.)"],
+			desc:
+				"I'm currently a junior at Tennessee Tech University majoring in computer science; I transferred half-way from a 2 year college at which I majored in Information Management Systems.",
+			link: ["Homepage", "https://tntech.edu"]
+		}
+	],
 
-   // Projects data list
-   // Each data: id, name, url, image url & description
-   proj_data: [
-      {
-         name: "Newsfeed",
-         url: "https://oneminch.github.io/Newsfeed/",
-         app_icon: "img/logos/newsfeed.svg",
-         desc: "A news app that loads the latest 20 headlines from an API based on a category selected."
-      },
-      {
-         name: "Encrypted List",
-         url: "https://oneminch.github.io/encryptedlist/",
-         app_icon: "img/logos/encryptedlist.svg",
-         desc: "A collective list of apps and services offering end-to-end encryption with filtering options."
-      },
-      {
-         name: "Dolist",
-         url: "https://oneminch.github.io/Dolist/",
-         app_icon: "img/logos/dolist.svg",
-         desc: "A to-do app build with jQuery with ability to add, complete, edit and delete tasks (First JavaScript Project)"
-      }
-   ]
+	// Projects data list
+	// Each data: id, name, url, image url & description
+	proj_data: [
+		{
+			name: "Newsfeed",
+			url: "https://oneminch.github.io/Newsfeed/",
+			app_icon: "img/logos/newsfeed.svg",
+			desc:
+				"A news app that loads the latest 20 headlines from an API based on a category selected."
+		},
+		{
+			name: "Encrypted List",
+			url: "https://oneminch.github.io/encryptedlist/",
+			app_icon: "img/logos/encryptedlist.svg",
+			desc:
+				"A collective list of apps and services offering end-to-end encryption with filtering options."
+		},
+		{
+			name: "Dolist",
+			url: "https://oneminch.github.io/Dolist/",
+			app_icon: "img/logos/dolist.svg",
+			desc:
+				"A to-do app build with jQuery with ability to add, complete, edit and delete tasks (First JavaScript Project)"
+		}
+	]
 };
 
 // Renders text with current year to footer
 const footerText = () => {
-   let d = new Date();
+	let d = new Date();
 
-   let markup = `
+	let markup = `
       Designed && Coded by Me
       <br>
       &copy; ${d.getFullYear()}
    `;
-   footer_text.innerHTML = markup;
+	footer_text.innerHTML = markup;
 };
 
 // Returns experience data markup: text, description + link(if applicable)
 const loadExpData = (id) => {
-   let markup = ``;
-   const exp_data_list = data_json.exp_data;
+	let markup = ``;
+	const exp_data_list = data_json.exp_data;
 
-   exp_data_list.forEach(item => {
-      // if id matches, fill basic data
-      if (item.id === id) {
-         markup = `
+	exp_data_list.forEach((item) => {
+		// if id matches, fill basic data
+		if (item.id === id) {
+			markup = `
             <h2 class="experience__current__title">${item.name}</h2>
             <h3 class="experience__current__date">(${item.date[0]} - ${item.date[1]})</h3>
             <div class="experience__current__desc">
@@ -89,45 +98,45 @@ const loadExpData = (id) => {
             </div>
          `;
 
-         // Hides button if there's no link
-         if (item.link.length === 0) {
-            markup += `<a href="#" class="experience__current__link experience__current__link--inexistent">Inexistent</a>`;
-         } else {
-            markup += `<a href="${item.link[1]}" target="_blank" class="experience__current__link">${item.link[0]}</a>`;
-         }
-      }
-   });
-   return markup;
+			// Hides button if there's no link
+			if (item.link.length === 0) {
+				markup += `<a href="#" class="experience__current__link experience__current__link--inexistent">Inexistent</a>`;
+			} else {
+				markup += `<a href="${item.link[1]}" target="_blank" class="experience__current__link">${item.link[0]}</a>`;
+			}
+		}
+	});
+	return markup;
 };
 
 // Renders experience from given id to container
 const renderExp = (id, container) => {
-   let curr_exp_markup = loadExpData(id);
-   container.innerHTML = curr_exp_markup;
+	let curr_exp_markup = loadExpData(id);
+	container.innerHTML = curr_exp_markup;
 };
 
 // Load footer text & renders current experience on load
-window.addEventListener('load', () => {
-   // Render footer text
-   footerText();
+window.addEventListener("load", () => {
+	// Render footer text
+	footerText();
 
-   // Highlights current experience indicator circle
-   const curr_exp_indicator = all_exps[all_exps.length-1];
-   curr_exp_indicator.classList.add("experience__list__item--active");
-   
-   // Current experience from json file
-   const exp_obj_list = data_json.exp_data;
-   const curr_exp_obj = exp_obj_list[exp_obj_list.length-1];
+	// Highlights current experience indicator circle
+	const curr_exp_indicator = all_exps[all_exps.length - 1];
+	curr_exp_indicator.classList.add("experience__list__item--active");
 
-   renderExp(curr_exp_obj.id, current_exp);
-   renderProjectData(data_json.proj_data);
+	// Current experience from json file
+	const exp_obj_list = data_json.exp_data;
+	const curr_exp_obj = exp_obj_list[exp_obj_list.length - 1];
+
+	renderExp(curr_exp_obj.id, current_exp);
+	renderProjectData(data_json.proj_data);
 });
 
 // Renders projects to UI
 const renderProjectData = (projectList) => {
-   let markup = ``;
-   for (let i = 0; i < projectList.length; i++) {
-      markup += `
+	let markup = ``;
+	for (let i = 0; i < projectList.length; i++) {
+		markup += `
          <a href="${projectList[i].url}" target="_blank" class="projects__project">
             <div class="icon">
                <img src="${projectList[i].app_icon}" alt="Project Icon"/>
@@ -136,26 +145,28 @@ const renderProjectData = (projectList) => {
             <div class="desc">${projectList[i].desc}</div>
          </a>
       `;
-   }
-   projects__container.innerHTML = markup;
+	}
+	projects__container.innerHTML = markup;
 };
 
 // Switch to different experiences with timeline
-all_exps.forEach(el => el.addEventListener('click', (e) => {
-   let active_exp = document.querySelector(".experience__list__item--active"); // Currently active experience
-   const clicked_exp = e.target.closest(".experience__list__item");
-   const exp_id = parseInt(clicked_exp.dataset.exp_id);
-   
-   if (!clicked_exp.classList.contains("experience__list__item--active")) {
-      // Adds active class to clicked experience circle in timeline
-      active_exp.classList.remove("experience__list__item--active");
-      clicked_exp.classList.add("experience__list__item--active");
-      
-      // Loads experience data correponding clicked circle
-      const exp_container = clicked_exp.parentElement.parentElement.previousElementSibling;
-      renderExp(exp_id, exp_container);
-   }
-}));
+all_exps.forEach((el) =>
+	el.addEventListener("click", (e) => {
+		let active_exp = document.querySelector(
+			".experience__list__item--active"
+		); // Currently active experience
+		const clicked_exp = e.target.closest(".experience__list__item");
+		const exp_id = parseInt(clicked_exp.dataset.exp_id);
 
+		if (!clicked_exp.classList.contains("experience__list__item--active")) {
+			// Adds active class to clicked experience circle in timeline
+			active_exp.classList.remove("experience__list__item--active");
+			clicked_exp.classList.add("experience__list__item--active");
 
-
+			// Loads experience data correponding clicked circle
+			const exp_container =
+				clicked_exp.parentElement.parentElement.previousElementSibling;
+			renderExp(exp_id, exp_container);
+		}
+	})
+);
